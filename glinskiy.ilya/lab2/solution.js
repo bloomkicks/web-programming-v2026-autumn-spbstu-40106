e0e686b930a977cf6a4f3eebe4f5ebc98bbfa7c1
Глинский Илья <glinckyweb@gmail.com>
2026-09-07 15:38:07 +0300

    [TASK-2] variant_34 glinskiy.ilya

---
+++ glinskiy.ilya/lab2/solution.js
@@ -0,0 +1,23 @@
+const open = '({[';
+const closed = ')}]';
+
+function isBalanced(str) {
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const curBracket = str[i];
+
+    if (open.includes(curBracket)) {
+      stack.push(curBracket);
+    } else if (closed.includes(curBracket)) {
+      const lastBracket = stack.pop();
+      if (open.indexOf(lastBracket) !== closed.indexOf(curBracket)) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+export {isBalanced};
